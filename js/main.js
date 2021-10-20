70718db7cf522d06ee5317c4e3928c9f20c1859a
doggyB12 <90135567+doggyB12@users.noreply.github.com>
2021-10-20 15:21:09 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,10 +22,10 @@ function firstQuestion(){
     Swal.fire({
         title: CONFIG.introTitle,
         text: CONFIG.introDesc,
-        imageUrl: 'img/meow.jpg',
+        imageUrl: 'img/bjas.jpg',
         imageWidth: 300,
         imageHeight: 300,
-        background: '#fff url("img/iput-bg.jpg")',
+        background: '#fff url("img/iput-assd.jpg")',
         imageAlt: 'Custom image',
         confirmButtonText: CONFIG.btnIntro
       }).then(function(){
